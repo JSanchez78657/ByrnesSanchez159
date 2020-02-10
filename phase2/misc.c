cc7c9e75f32f30a0ca475b46129bf1241e2c2011
JSanchez78657 <mrkarato@gmail.com>
2020-02-10 14:23:37 -0800

    Began changes to proc and misc, including moving str_len and itos to misc.c

--- phase2/misc.c
+++ phase2/misc.c
@@ -63,4 +63,23 @@ void MemCpy(char *source, char *dest, unsigned nBytes)
 	}
 }
 
+int str_len(char* s) {
+    int i;
+    for(i = 0; s[i] != '\0'; ++i);
+    return i;
+}
 
+void itos(int i, char*s) {
+    int h;
+    h = str_len(s) - 1;
+    if(i == 0) s[h] = '0';
+    else for(; i > 0; i /= 10)
+        s[h--] = '0' + (i %10);
+}
+
+void StrAdd(char* str, char c) {
+    int i;
+    for(i = 0; str[i] != '\0'; ++i);
+    str[i+1] = '\0';
+    str[i] = c;
+}
